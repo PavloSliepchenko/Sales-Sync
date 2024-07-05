604afdf2473962742b2f8c3c057a5afd5a465e7b
PavloSliepchenko <sliepchenkopavlo@gmail.com>
2024-07-05 07:35:31 -0500

    first commit

---
+++ src/main/java/com/example/salessync/SalesSyncApplication.java
@@ -0,0 +1,13 @@
+package com.example.salessync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesSyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/salessync/SalesSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salessync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
