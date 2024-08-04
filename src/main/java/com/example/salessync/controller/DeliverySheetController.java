53ec733258075bf3ab382e15fdb366befe660f02
PavloSliepchenko <sliepchenkopavlo@gmail.com>
2024-08-04 12:52:19 -0500

    Added delivery sheet and delivery sheet line controllers, edited description of methods in supply sheet and supply sheet line controllers, fixed issues in delivery sheet line service

--- src/main/java/com/example/salessync/controller/DeliverySheetController.java
+++ src/main/java/com/example/salessync/controller/DeliverySheetController.java
@@ -1,8 +1,8 @@
 package com.example.salessync.controller;
 
-import com.example.salessync.dto.sheet.SupplySheetResponseDto;
+import com.example.salessync.dto.sheet.DeliverySheetResponseDto;
 import com.example.salessync.model.User;
-import com.example.salessync.service.SupplySheetService;
+import com.example.salessync.service.DeliverySheetService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -14,17 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/supplySheets")
-@Tag(name = "Supply sheets management",
-        description = "Provides endpoints for CRUD operations with supply sheets")
-public class SupplySheetController {
-    private final SupplySheetService sheetService;
+@RequestMapping(value = "/deliverySheets")
+@Tag(name = "Delivery sheets management",
+        description = "Provides endpoints for CRUD operations with delivery sheets")
+public class DeliverySheetController {
+    private final DeliverySheetService sheetService;
 
     @GetMapping
     @PreAuthorize("hasAuthority('USER')")
-    @Operation(summary = "Get supply sheet",
+    @Operation(summary = "Get delivery sheet",
             description = "Allows to get sheet by user ID. Available to all authenticated users")
-    public SupplySheetResponseDto getSheetById(Authentication authentication) {
+    public DeliverySheetResponseDto getSheetById(Authentication authentication) {
         User user = (User) authentication.getPrincipal();
         return sheetService.getSheet(user.getId());
     }
--- src/main/java/com/example/salessync/controller/DeliverySheetLineController.java
+++ src/main/java/com/example/salessync/controller/DeliverySheetLineController.java
@@ -1,9 +1,9 @@
 package com.example.salessync.controller;
 
-import com.example.salessync.dto.line.CreateSupplySheetLineRequestDto;
-import com.example.salessync.dto.line.SupplySheetLineResponseDto;
+import com.example.salessync.dto.line.CreateDeliverySheetLineRequestDto;
+import com.example.salessync.dto.line.DeliverySheetLineResponseDto;
 import com.example.salessync.model.User;
-import com.example.salessync.service.SupplySheetLineService;
+import com.example.salessync.service.DeliverySheetLineService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -22,20 +22,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/supplyLines")
-@Tag(name = "Supply sheet lines management",
-        description = "Provides endpoints for CRUD operations with supply sheet lines")
-public class SupplySheetLineController {
-    private final SupplySheetLineService lineService;
+@RequestMapping(value = "/deliveryLines")
+@Tag(name = "Delivery sheet lines management",
+        description = "Provides endpoints for CRUD operations with delivery sheet lines")
+public class DeliverySheetLineController {
+    private final DeliverySheetLineService lineService;
 
     @PostMapping
     @PreAuthorize("hasAuthority('USER')")
     @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "Add a new supply sheet line",
+    @Operation(summary = "Add a new delivery sheet line",
             description = "Allows to add a new line. Available to all authenticated users")
-    public SupplySheetLineResponseDto addLine(
+    public DeliverySheetLineResponseDto addLine(
             Authentication authentication,
-            @Valid @RequestBody CreateSupplySheetLineRequestDto requestDto
+            @Valid @RequestBody CreateDeliverySheetLineRequestDto requestDto
     ) {
         User user = (User) authentication.getPrincipal();
         return lineService.addLine(user.getId(), requestDto);
@@ -43,12 +43,12 @@ public class SupplySheetLineController {
 
     @PatchMapping
     @PreAuthorize("hasAuthority('USER')")
-    @Operation(summary = "Update a supply sheet line",
+    @Operation(summary = "Update a delivery sheet line",
             description = "Allows to update (patch) line. Available to all authenticated users")
-    public SupplySheetLineResponseDto updateLine(
+    public DeliverySheetLineResponseDto updateLine(
             Authentication authentication,
             @RequestParam Long lineId,
-            @RequestBody CreateSupplySheetLineRequestDto requestDto
+            @RequestBody CreateDeliverySheetLineRequestDto requestDto
     ) {
         User user = (User) authentication.getPrincipal();
         return lineService.updateLine(user.getId(), lineId, requestDto);
@@ -57,7 +57,7 @@ public class SupplySheetLineController {
     @DeleteMapping
     @PreAuthorize("hasAuthority('USER')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @Operation(summary = "Delete a supply sheet line by ID",
+    @Operation(summary = "Delete a delivery sheet line by ID",
             description = "Allows to delete line by ID. Available to all authenticated users")
     public void deleteLine(Authentication authentication,
                            @RequestParam Long lineId) {
